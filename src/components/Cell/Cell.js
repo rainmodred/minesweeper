539d3617da5bf30df63e794e4bf0f0ccda0a9ea0
rainmodred <albphoenix@gmail.com>
2019-05-08 17:14:26 +0300

    add cell styles

--- src/components/Cell/Cell.js
+++ src/components/Cell/Cell.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import bomb from '../../assets/bomb.png';
+
+const colors = {
+  '1': '#0000ff',
+  '2': '#008200',
+  '3': '#ff0000',
+  '4': '#000084',
+  '5': '#840000',
+  '6': '#008284',
+  '7': '#840084',
+  '8': '#757575',
+};
+
 const StyledCell = styled.div`
+  font-family: 'Changa One', sans-serif;
+  font-size: 24px;
   width: 24px;
   height: 24px;
+  font-weight: 400;
+  line-height: 24px;
+  text-align: center;
+`;
+
+const ClosedCell = styled(StyledCell)`
   border-top: 3px solid #fff;
   border-left: 3px solid #fff;
   border-bottom: 3px solid #808080;
@@ -15,8 +36,26 @@ const StyledCell = styled.div`
   }
 `;
 
-const Cell = () => {
-  return <StyledCell>{/*  */}</StyledCell>;
+const OpenedCell = styled(StyledCell)` 
+  border: none;
+  border-top: 1px solid #808080;
+  border-left: 1px solid #808080;
+  background-image: url('${props => (props.hasMine ? bomb : null)}');
+  background-repeat: no-repeat;
+  background-size: cover;
+  color: ${props => colors[`${props.neigbours}`]};
+`;
+
+const Cell = ({ hasMine, isOpened, neighbourMineCount, row, col }) => {
+  let neigbours = hasMine || neighbourMineCount === 0 ? null : neighbourMineCount;
+  let cell = isOpened ? (
+    <OpenedCell neigbours={neigbours} hasMine={hasMine}>
+      {neigbours}
+    </OpenedCell>
+  ) : (
+    <ClosedCell />
+  );
+  return cell;
 };
 
 export default Cell;
