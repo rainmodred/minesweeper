b5d5cafe4cd75df726dbfc7bc88141016cb8a23a
rainmodred <albphoenix@gmail.com>
2019-05-11 17:42:36 +0300

    add header timer and styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
+import Digit from '../Digit/Digit';
+
+import face from '../../assets/face_1.png';
+
 const StyledHeader = styled.div`
   display: flex;
   justify-content: space-between;
@@ -15,10 +19,15 @@ const StyledHeader = styled.div`
   padding: 12px;
 `;
 
-const DigitPlaceholder = styled.div`
+const DigitsContainer = styled.div`
   background-color: #000;
   height: 35px;
   width: 60px;
+  display: inline-block;
+  white-space: nowrap;
+  padding: 2px;
+  display: flex;
+  align-items: center;
 `;
 
 const SmilePlaceholder = styled.div`
@@ -28,17 +37,72 @@ const SmilePlaceholder = styled.div`
   border-left: 4px solid #fff;
   border-bottom: 5px solid #808080;
   border-right: 5px solid #808080;
-  background: url('./5965934191706112.png') 0 -16px no-repeat;
-  background-size: 50% 50%;
+  background: url('${face}')no-repeat;
+  background-size: 30px;
+  background-position: center;
+
 `;
 
 class Header extends Component {
+  state = {
+    seconds: 0,
+  };
+
+  startTimer = () => {
+    setInterval(
+      () =>
+        this.setState(prevState => {
+          return prevState.seconds++;
+        }),
+      1000
+    );
+    if (this.state.seconds < 10) {
+      this.setState(prevState => {
+        return prevState.third++;
+      });
+    }
+  };
+  componentDidMount() {
+    this.startTimer();
+  }
+
   render() {
+    const seconds = this.state.seconds.toString().split('');
+    let third = seconds[0] !== undefined ? seconds[0] : 0;
+    let first = seconds[2] !== undefined ? seconds[2] : 0;
+    let second = seconds[1] !== undefined ? seconds[1] : 0;
+    if (seconds.length === 1) {
+      third = seconds[0] !== undefined ? seconds[0] : 0;
+      first = 0;
+      second = 0;
+    }
+
+    if (seconds.length === 2) {
+      third = seconds[1] !== undefined ? seconds[1] : 0;
+      first = 0;
+      second = seconds[0] !== undefined ? seconds[0] : 0;
+    }
+
+    if (seconds.length === 3) {
+      third = seconds[2] !== undefined ? seconds[2] : 0;
+
+      first = seconds[0] !== undefined ? seconds[0] : 0;
+      second = seconds[1] !== undefined ? seconds[1] : 0;
+    }
+
     return (
       <StyledHeader>
-        <DigitPlaceholder />
+        <DigitsContainer>
+          <Digit digit={0} />
+          <Digit digit={2} />
+          <Digit digit={1} />
+        </DigitsContainer>
         <SmilePlaceholder />
-        <DigitPlaceholder />
+        <DigitsContainer>
+          <Digit digit={first} />
+          <Digit digit={second} />
+          <Digit digit={third} />
+        </DigitsContainer>
       </StyledHeader>
     );
   }
