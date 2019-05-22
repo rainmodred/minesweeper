f4a6f131b9b9e84a1f491038d4aaffe753e172a5
rainmodred <albphoenix@gmail.com>
2019-05-22 17:12:46 +0300

    add difficulties

---
+++ src/difficulties.js
@@ -0,0 +1,19 @@
+const difficulties = {
+  Beginner: {
+    width: 9,
+    height: 9,
+    minesQuantity: 10,
+  },
+  Intermediate: {
+    width: 16,
+    height: 16,
+    minesQuantity: 40,
+  },
+  Expert: {
+    width: 30,
+    height: 16,
+    minesQuantity: 99,
+  },
+};
+
+export default difficulties;
